b512fcad183fddfa103c746f49d6f3e0cbe4a86b
Nanami <anhdao11a5cmb1920@gmail.com>
2023-03-20 22:37:35 +0700

    first commit

---
+++ BT1/BT1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+namespace BT1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Tính số phần tử tối đa của mảng
+            // Date of Birth: 19/06/2003
+            int max = 19 + 6 + 3;
+            double[] arr = new double[max];
+
+            Console.Write("Nhap so phan tu cua mang: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Nhap gia tri phan tu thu {0}: ", i);
+                arr[i] = double.Parse(Console.ReadLine());
+            }
+
+            // in ra cac phan tu co phan nguyen chia het cho 6
+            int sum = 6;
+            Console.WriteLine("Cac phan tu co phan nguyen chia het cho {0}:", sum);
+            for (int i = 0; i < n; i++)
+            {
+                if ((int)arr[i] % sum == 0)
+                {
+                    Console.Write("{0}\t", arr[i]);
+                }
+            }
+
+            // Kiểm tra phần tử x có trong danh sách đã nhập hay không
+            Console.Write("\nNhap gia tri x can kiem tra: ");
+            double x = double.Parse(Console.ReadLine());
+            bool isFound = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == x)
+                {
+                    isFound = true;
+                    break;
+                }
+            }
+            if (isFound)
+            {
+                Console.WriteLine("Gia tri {0} co trong danh sach.", x);
+            }
+            else
+            {
+                Console.WriteLine("Gia tri {0} khong co trong danh sach.", x);
+            }
+
+            // Đếm số lần xuất hiện của phần tử y trong danh sách đã nhập
+            Console.Write("Nhap gia tri y can dem: ");
+            double y = double.Parse(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] == y)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Gia tri {0} xuat hien {1} lan trong danh sach.", y, count);
+        }
+    }
+}
---
+++ BT2/BT2.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace BT2
+{
+    class Student
+    {
+        private double score { get; set; }
+        private string id { get; set; }
+        private string name { get; set; }
+
+        public Student() {}
+
+        public Student(double score, string id, string name)
+        {
+            this.score = score;
+            this.id = "SV" + id;
+            this.name = name;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("Student ID: {0}", id);
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Score: {0}", score);
+        }
+    }
+
+    class subStudent1 : Student
+    {
+        private int age { get; set; }
+        public subStudent1(double score, string id, string name, int age) : base(score, id, name)
+        {
+            this.age = age;
+        }
+
+        public new void display()
+        {
+            base.display();
+            Console.WriteLine("Age: {0}", age);
+        }
+    }
+
+    class subStudent2 : Student
+    {
+        private string email { get; set; }
+
+        private static int count = 0;
+
+        public subStudent2() : base()
+        {
+            email = "";
+            count++;
+        }
+
+        public subStudent2(double score, string id, string name, string email) : base(score, id, name)
+        {
+            this.email = email;
+            count++;
+        }
+
+        public static int Count()
+        {
+            return count;
+        }
+
+        public new void display()
+        {
+            base.display();
+            Console.WriteLine("Email: {0}", email);
+
+        }
+    }
+
+    class BT2
+    {
+        public static void Main(string[] args)
+        {
+            Student student1 = new Student(10, "21012864", "Bui Thi Anh Dao");
+            subStudent1 student2 = new subStudent1(10, "21012864", "Bui Thi Anh Dao", 19);
+            subStudent2 student3 = new subStudent2(10, "21012864", "Bui Thi Anh Dao", "anhdao19623@gmail.com");
+
+            student1.display();
+            Console.WriteLine();
+
+            student2.display();
+        
+            Console.WriteLine();
+
+            student3.display();
+            Console.WriteLine(subStudent2.Count());
+        
+        }
+    }
+}
